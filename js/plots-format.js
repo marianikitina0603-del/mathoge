46eac4a801f44b3883c4cf6434e6525435e251a2
marianikitina0603-del <marianikitina0603@gmail.com>
2026-08-28 20:32:41 +0300

    Refresh unified OGE 8 fraction formatting

--- js/plots-format.js
+++ js/plots-format.js
@@ -22,8 +22,8 @@
     await load('js/number7-title.js?v=20260828-1940');
     await load('js/number8-data.js?v=20260828-2045');
     await load('js/number8-analogs.js?v=20260828-2115');
-    await load('js/number8-fractions.js?v=20260828-2145');
-    await load('js/empty-numbers.js?v=20260828-2145');
+    await load('js/number8-fractions.js?v=20260828-2205');
+    await load('js/empty-numbers.js?v=20260828-2205');
     if(typeof window.renderBank==='function')window.renderBank(); if(typeof window.renderBuilderBank==='function')window.renderBuilderBank();
     if(typeof window.refreshApartmentPlans==='function')window.refreshApartmentPlans(); if(typeof window.refreshSheetPlans==='function')window.refreshSheetPlans(); if(typeof window.refreshStoveTasks==='function')window.refreshStoveTasks(); if(typeof window.refreshTariffPlans==='function')window.refreshTariffPlans(); if(typeof window.refreshTirePlans==='function')window.refreshTirePlans(); if(typeof window.updateCounters==='function')window.updateCounters();
   }catch(e){console.error('Не удалось загрузить дополнительные разделы банка',e);}})();
