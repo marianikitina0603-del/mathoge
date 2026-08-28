7db0cd7a2d7e608b2c57ea4b92044a069e558c3b
marianikitina0603-del <marianikitina0603@gmail.com>
2026-08-28 21:02:10 +0300

    Refresh LaTeX formatting for all OGE 8 powers

--- js/plots-format.js
+++ js/plots-format.js
@@ -22,9 +22,9 @@
     await load('js/number7-title.js?v=20260828-1940');
     await load('js/number8-data.js?v=20260828-2045');
     await load('js/number8-analogs.js?v=20260828-2115');
-    await load('js/number8-latex.js?v=20260828-2055');
+    await load('js/number8-latex.js?v=20260828-2105');
     await load('js/number8-fractions.js?v=20260828-2049');
-    await load('js/empty-numbers.js?v=20260828-2055');
+    await load('js/empty-numbers.js?v=20260828-2105');
     if(typeof window.renderBank==='function')window.renderBank(); if(typeof window.renderBuilderBank==='function')window.renderBuilderBank();
     if(typeof window.typesetMathOGE==='function')window.typesetMathOGE();
     if(typeof window.refreshApartmentPlans==='function')window.refreshApartmentPlans(); if(typeof window.refreshSheetPlans==='function')window.refreshSheetPlans(); if(typeof window.refreshStoveTasks==='function')window.refreshStoveTasks(); if(typeof window.refreshTariffPlans==='function')window.refreshTariffPlans(); if(typeof window.refreshTirePlans==='function')window.refreshTirePlans(); if(typeof window.updateCounters==='function')window.updateCounters();
