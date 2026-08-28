24998b94363411f28d222d7e657dd7463126878c
marianikitina0603-del <marianikitina0603@gmail.com>
2026-08-28 20:57:36 +0300

    Refresh MathJax renderer for OGE 8 roots

--- js/plots-format.js
+++ js/plots-format.js
@@ -22,11 +22,11 @@
     await load('js/number7-title.js?v=20260828-1940');
     await load('js/number8-data.js?v=20260828-2045');
     await load('js/number8-analogs.js?v=20260828-2115');
-    await load('js/number8-latex.js?v=20260828-2049');
+    await load('js/number8-latex.js?v=20260828-2055');
     await load('js/number8-fractions.js?v=20260828-2049');
-    await load('js/empty-numbers.js?v=20260828-2049');
+    await load('js/empty-numbers.js?v=20260828-2055');
     if(typeof window.renderBank==='function')window.renderBank(); if(typeof window.renderBuilderBank==='function')window.renderBuilderBank();
-    if(window.MathJax&&MathJax.typesetPromise) MathJax.typesetPromise().catch(console.error);
+    if(typeof window.typesetMathOGE==='function')window.typesetMathOGE();
     if(typeof window.refreshApartmentPlans==='function')window.refreshApartmentPlans(); if(typeof window.refreshSheetPlans==='function')window.refreshSheetPlans(); if(typeof window.refreshStoveTasks==='function')window.refreshStoveTasks(); if(typeof window.refreshTariffPlans==='function')window.refreshTariffPlans(); if(typeof window.refreshTirePlans==='function')window.refreshTirePlans(); if(typeof window.updateCounters==='function')window.updateCounters();
   }catch(e){console.error('Не удалось загрузить дополнительные разделы банка',e);}})();
 })();
