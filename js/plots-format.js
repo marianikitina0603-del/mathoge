84c83e9c6dabadc54e1188ba537e4d7fb2b4bd02
marianikitina0603-del <marianikitina0603@gmail.com>
2026-08-29 21:55:45 +0300

    Add FIPI task bank for number 14

--- js/plots-format.js
+++ js/plots-format.js
@@ -26,7 +26,8 @@
       chain('js/number10-data.js?v=20260829-n10-1','js/number10-analogs.js?v=20260829-n10-1'),
       chain('js/number11-data.js?v=20260829-n11-answer-table-1','js/number11-analogs.js?v=20260829-n11-1'),
       chain('js/number12-data.js?v=20260829-n12-1','js/number12-analogs.js?v=20260829-n12-1'),
-      chain('js/number13-data.js?v=20260829-n13-svg-2','js/number13-analogs.js?v=20260829-n13-svg-2')
+      chain('js/number13-data.js?v=20260829-n13-svg-2','js/number13-analogs.js?v=20260829-n13-svg-2'),
+      chain('js/number14-data.js?v=20260829-n14-1','js/number14-analogs.js?v=20260829-n14-1')
     ]);
     await load('js/empty-numbers.js?v=20260828-2125');
     if(typeof window.hydrateVariant==='function')window.hydrateVariant();
