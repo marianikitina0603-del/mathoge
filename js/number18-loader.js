7f7f751a9aa5f7ba725859ca1f19d69ce1e4e439
marianikitina0603-del <marianikitina0603@gmail.com>
2026-08-30 19:19:52 +0300

    Подключить фикс рисунков для печати с решением

--- js/number18-loader.js
+++ js/number18-loader.js
@@ -7,6 +7,7 @@
       await load('js/number18-analogs.js?v=20260830-n18-1');
       await load('js/number18-analogs-part2.js?v=20260830-n18-1');
       await load('js/number18-analogs-part3.js?v=20260830-n18-1');
+      await load('js/ordinary-print-image-fix.js?v=20260830-print-images-1');
       if(typeof window.hydrateVariant==='function')window.hydrateVariant();
       if(typeof window.renderBank==='function')window.renderBank();
       if(typeof window.renderBuilderBank==='function')window.renderBuilderBank();
